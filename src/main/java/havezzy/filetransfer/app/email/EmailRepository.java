fab20d21856da3f6e256e9b7c73f5d070b38753a
davidbgroupsolums <159963360+davidbgroupsolums@users.noreply.github.com>
2024-02-13 22:45:59 +0000

    best

--- src/main/java/havezzy/filetransfer/app/email/EmailRepository.java
+++
@@ -1,9 +0,0 @@
-package havezzy.filetransfer.app.email;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmailRepository extends CrudRepository<Email, Integer> {
-
-}
